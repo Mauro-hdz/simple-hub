0b4b441fa8c91b69a0702dd6d0170083b630a064
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2020-02-02 23:53:16 -0600

    basic api concept works

--- routes/api.py
+++ routes/api.py
@@ -1,5 +1,28 @@
-
+from flask import jsonify
 def api(app):
-    @app.route('/api')
+    @app.route('/api/dog')
     def dictionary():
-        return 'hello world'
+        dog = {
+            'dog': 'pitbull',
+            'age': 3,
+            'color': 'brown'
+        }
+        return jsonify(dog)
+
+    @app.route('/api/cat')
+    def cat():
+        cat = {
+            'cat': 'lynx',
+            'age': 2,
+            'color': 'white'
+        }
+        return jsonify(cat)
+
+    @app.route('/api/bird')
+    def bird():
+        bird = {
+            'bird': 'canary',
+            'age': 1,
+            'color': 'yellow'
+        }
+        return jsonify(bird)
