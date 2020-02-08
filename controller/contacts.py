5f7c831241e40a1b663cc0b0fc1eda35deae8030
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2020-02-08 12:03:04 -0600

    contact api in progress

--- controller/contacts.py
+++ controller/contacts.py
@@ -1,25 +1,59 @@
-from flask import jsonify
+from flask import jsonify, request
+from models.contact import Contact
 
-def meeting_api(app):
-    @app.route('/api/contact/all')
-    def fish():
-        fish = {
-            'type': 'goldfish'
-        }
-        return jsonify(fish)
+def contact_api(app, session):
+    @app.route('/api/contact/one/<id>', methods=['GET'])
+    def one_contact(id):
+        if request.method == 'GET':
+            instance_found = session.query(Contact).filter_by(id=id).first()
+            response = {
+                'id': instance_found.id,
+                'name': instance_found.name,
+                'title': instance_found.title,
+                'email': instance_found.email,
+                'phoneNumber': instance_found.phone_number,
+                'note': instance_found.note
+            }
+            print(response, 'Instance was found')
+            return jsonify(response)
+
+
+    @app.route('/api/contact/all', methods=['GET'])
+    def all_contacts():
+        if request.method == 'GET':
+            response = {
+                'data': []
+            }
+            for instance in session.query(Contact).all():
+                contact_instance = {
+                    'name': instance.name,
+                    'title': instance.title,
+                    'email': instance.email,
+                    'phoneNumber': instance.phone_number,
+                    'note': instance.note
+                }
+                response['data'].append(contact_instance)
+            print(response, 'All instances were found.')
+            return jsonify(fish)
     
 
-    @app.route('/api/contact/add')
-    def cat():
+    @app.route('/api/contact/add', methods=['POST'])
+    def add_contact():
         cat = {
             'cat': 'lynx',
             'age': 2,
             'color': 'white'
         }
         return jsonify(cat)
+        
 
-    @app.route('/api/contact/delete/<id>')
-    def bird(id):
+    @app.route('/api/contact/update/<id>', methods=['PUT'])
+    def update_contact():
+        return 'Update route'
+
+
+    @app.route('/api/contact/delete/<id>', methods=['DELETE'])
+    def delete_contact():
         bird = {
             'id': id,
             'bird': 'canary',
