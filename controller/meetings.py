e2ee9fbdec517a91c0eb8f42dcb170fadd3ca832
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2020-02-07 18:43:41 -0600

    meetings api is complete

--- controller/meetings.py
+++ controller/meetings.py
@@ -2,14 +2,39 @@ from flask import jsonify, request
 from models.meeting import Meeting
 
 def meeting_api(app, session):
+    @app.route('/api/meeting/one/<id>', methods=['GET'])
+    def one_meeting(id):
+        if request.method == 'GET':
+           for keys in session.query(Meeting).filter(Meeting.id == id):
+               response = {
+                   'subject': keys.subject,
+                   'location': keys.location,
+                   'date': keys.date,
+                   'time': keys.time,
+                   'attending': keys.attending,
+                   'id': keys.id
+               }
+               print(response, 'Instance was found.')
+               return jsonify(response)
+           
     @app.route('/api/meeting/all', methods=['GET'])
     def all_meetings():
         if request.method == 'GET':
-            fish = {
-                'type': 'golfish',
-                'color': 'yellow'
+            response = {
+                'data': []
             }
-            return jsonify(fish)
+            for meeting in session.query(Meeting).all():
+                meeting_instance = {
+                    'subject': meeting.subject,
+                    'location': meeting.location,
+                    'date': meeting.date,
+                    'time': meeting.time,
+                    'attending': meeting.attending,
+                    'id': meeting.id
+                }
+                response['data'].append(meeting_instance)
+            print(response, 'All instances were found.')
+            return jsonify(response)
     
 
     @app.route('/api/meeting/add', methods=['POST'])
@@ -34,12 +59,10 @@ def meeting_api(app, session):
             print(meeting, 'Added To The Database Successfully.')
             return jsonify(meeting)
 
-    @app.route('/api/meeting/delete/<id>')
+    @app.route('/api/meeting/delete/<id>', methods=['DELETE'])
     def delete_meeting(id):
-        bird = {
-            'id': id,
-            'bird': 'canary',
-            'age': 1,
-            'color': 'yellow'
-        }
-        return jsonify(bird)
+        if request.method == 'DELETE':
+            selected_meeting = session.query(Meeting).filter_by(id=id).delete()
+            session.commit()
+            print('Meeting with id: {id}, was deleted.'.format(id=id))
+            return f'Meeting with id: {id}, was deleted.'
