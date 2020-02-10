4e043f27bcc0abb79c1aace383434ae283ff49f5
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2020-02-10 16:37:30 -0600

    meeting post working

--- client/src/components/meetingModal.js
+++ client/src/components/meetingModal.js
@@ -1,25 +1,104 @@
 import React, { Component } from 'react';
 import { Button, Modal, Form, Icon, List} from 'semantic-ui-react';
 import InputMask from 'react-input-mask';
+import axios from 'axios';
 
 class MeetingModal extends Component {
-    state = {
-        person: '',
-        people: []
+    constructor(props) {
+        super(props);
+
+        this.onClickAddPerson = this.onClickAddPerson.bind(this);
+        this.onChangePerson = this.onChangePerson.bind(this);
+        this.onClickRemovePerson = this.onClickRemovePerson.bind(this);
+        this.onChangeSubject = this.onChangeSubject.bind(this);
+        this.onChangeLocation = this.onChangeLocation.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onChangeHour = this.onChangeHour.bind(this);
+        this.onChangeAmPm = this.onChangeAmPm.bind(this);
+        this.onClickSubmit = this.onClickSubmit.bind(this);
+
+        this.state = {
+            subject: '',
+            location: '',
+            date: '',
+            time: '',
+            hour: '',
+            AmPm: '',
+            person: '',
+            people: []
+        }
+    }
+
+    onClickAddPerson = (event) => {
+        this.setState({
+            people: [...this.state.people, this.state.person],
+            person: ''
+        });
     };
 
-onClickAddPerson = (event) => {
-    event.preventDefault();
-    this.setState({
-        people: [...this.state.people, this.state.person],
-        person: ''
-    });
-};
+    onChangePerson = (e) => {
+        this.setState({person: e.target.value });
+    };
 
-onChangePerson = () => {
-    const personInput = document.getElementById('person-input').value; //grabs text input
-    this.setState({person: personInput });
-};
+    onClickRemovePerson(person) {
+        const index = this.state.people.findIndex(element => element === person)
+        this.state.people.splice(index, 1)
+        this.setState({
+            people: this.state.people
+        })
+        
+    };
+
+    onChangeSubject(e) {
+        this.setState({
+            subject: e.target.value
+        })
+    };
+
+    onChangeLocation(e) {
+        this.setState({
+            location: e.target.value
+        })
+    };
+
+    onChangeDate(e) {
+        this.setState({
+            date: e.target.value
+        })
+    };
+
+    onChangeHour(e) {
+        this.setState({
+            hour: e.target.value,
+            time: e.target.value + ' ' + this.state.AmPm
+        })
+    };
+
+    onChangeAmPm(e, {value}) {
+        console.log(value)
+        this.setState({
+            AmPm: value,
+            time: this.state.hour + ' ' + value
+        })
+    };
+
+    onClickSubmit(e) {
+        const meeting = {
+            subject: this.state.subject,
+            location: this.state.location,
+            date: this.state.date,
+            time: this.state.time,
+            attending: this.state.people.join(', ')
+        }
+        console.log(meeting)
+        axios.post('/api/meeting/add', JSON.stringify(meeting))
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
 
   render() {
     const timeOptions = [{text: 'AM', value: 'AM'}, {text: 'PM', value: 'PM'}]
@@ -30,23 +109,23 @@ onChangePerson = () => {
           <Modal.Content>
               <Form>
                   <Form.Group>
-                      <Form.Input label='Subject' placeholder='i.e consultation' />
+                      <Form.Input value={this.state.subject} onChange={this.onChangeSubject} label='Subject' placeholder='i.e consultation' />
                   </Form.Group>
                   <Form.Group>
-                          <Form.Input label='Location' placeholder='i.e Dallas, 123 Sesame St.' />
+                          <Form.Input value={this.state.location} onChange={this.onChangeLocation} label='Location' placeholder='i.e Dallas, 123 Sesame St.' />
                   </Form.Group>
                   <Form.Group>
                           <Form.Field>
                               <label>Date</label>
-                              <Form.Input className='ui input' as={InputMask} mask='99/99/9999' placeholder='01/15/2020' />
+                              <Form.Input value={this.state.date} onChange={this.onChangeDate} className='ui input' as={InputMask} mask='99/99/9999' placeholder='01/15/2020' />
                           </Form.Field>
                           <Form.Field>
                               <label>Time</label>
-                              <Form.Field as={InputMask} placeholder='i.e 04:29' mask='99:99' className='ui input' />
+                              <Form.Field value={this.state.hour} onChange={this.onChangeHour} as={InputMask} placeholder='i.e 04:29' mask='99:99' className='ui input' />
                           </Form.Field>
                           <Form.Field>
                               <label>AM/PM</label>
-                              <Form.Select placeholder='i.e PM' options={timeOptions} />
+                              <Form.Select onChange={this.onChangeAmPm} placeholder='i.e PM' options={timeOptions} />
                           </Form.Field>
                   </Form.Group>
                   <Form.Group inline>
@@ -60,8 +139,8 @@ onChangePerson = () => {
               <List>
                 {this.state.people.map(person => {
                     return (
-                    <List.Item>
-                        <List.Icon name='x' />
+                    <List.Item key={person}>
+                        <List.Icon value={person} color="red" onClick={() => this.onClickRemovePerson(person)} link name='x' />
                         <List.Content>{person}</List.Content>
                     </List.Item>
                     )
@@ -71,7 +150,7 @@ onChangePerson = () => {
                   {/* <Button color='red'>
                       <Icon name='remove' /> Cancel
                   </Button> */}
-                  <Button color='green'>
+                  <Button onClick={this.onClickSubmit} color='green'>
                       <Icon name='checkmark' /> Submit
                   </Button>
               </Modal.Actions>
