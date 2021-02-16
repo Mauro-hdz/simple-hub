a235dda9c306b6cd047c03a400cf01d63d4dedf9
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2021-02-15 20:25:11 -0600

    adding form for log in

--- client/src/App.js
+++ client/src/App.js
@@ -11,57 +11,69 @@ import EditContact from './pages/editContact';
 import LoginPage from "./pages/LoginPage";
 
 class App extends Component {
+	state = {
+		user: {
+			loggedIn: false,
+		},
+		visible: true,
+		container: 'no-left-margin',
+	};
 
-  state = {
-    user: {
-      loggedIn: true
-    },
-    visible: true,
-    container: 'no-left-margin'
-  };
+	onClick = () => {
+		if (this.state.visible === true) {
+			this.setState({ visible: false, container: 'ui container' });
+		} else {
+			this.setState({ visible: true, container: 'no-left-margin' });
+		}
+	};
 
-
-  onClick = () => {
-    if (this.state.visible === true) {this.setState({visible: false, container: 'ui container'})} 
-    else {this.setState({visible: true, container: 'no-left-margin'})}
-  };
-
-  render() {
-    return (
-		<div className='App'>
-			<Router>
-				<Switch>
-          {this.state.user.loggedIn?(
-            <>
-             <Nav onClick={this.onClick} />
-             <Sidebar
-             sidebarVisible={this.state.visible}
-             className={this.state.container}
-             >
-             <Route path='/contacts' exact component={Contacts} />
-             <Route path='/meetings' exact component={Meetings} />
-             <Route path='/tasks' exact component={Tasks} />
-             <Route
-               path='/edit/contact/:id'
-               exact
-               component={EditContact}
-             />
-             <Route
-               path='/edit/meeting/:id'
-               exact
-               component={EditMeeting}
-             />
-           </Sidebar>
-           </>
-          ): (
-          <Route path='/' exact component={LoginPage} />
-          )}
-				</Switch>
-			</Router>
-		</div>
-	);
-  }
-  
+	render() {
+		return (
+			<div className='App'>
+				<Router>
+					<Switch>
+						{this.state.user.loggedIn ? (
+							<>
+								<Nav onClick={this.onClick} />
+								<Sidebar
+									sidebarVisible={this.state.visible}
+									className={this.state.container}
+								>
+									<Route
+										path='/contacts'
+										exact
+										component={Contacts}
+									/>
+									<Route
+										path='/meetings'
+										exact
+										component={Meetings}
+									/>
+									<Route
+										path='/tasks'
+										exact
+										component={Tasks}
+									/>
+									<Route
+										path='/edit/contact/:id'
+										exact
+										component={EditContact}
+									/>
+									<Route
+										path='/edit/meeting/:id'
+										exact
+										component={EditMeeting}
+									/>
+								</Sidebar>
+							</>
+						) : (
+							<Route path='/' exact component={LoginPage} />
+						)}
+					</Switch>
+				</Router>
+			</div>
+		);
+	}
 }
 
 export default App;
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,14 +1,39 @@
 import React from "react";
-import { Form } from "semantic-ui-react";
+import {
+	Form,
+	FormInput,
+	Button,
+	Container,
+	GridColumn,
+	GridRow,
+	Grid,
+} from 'semantic-ui-react';
 
 function LoginPage() {
     return (
-        <div 
-        // style={{minHeight: "100vh"}}
-        >
-            <h1>Hello World</h1>
-        </div>
-    )
+		<div>
+			<Container
+				style={{
+					backgroundColor: 'grey',
+					minWidth: '100vw',
+					minHeight: '100vh',
+				}}
+			>
+				<Grid width={12}>
+					<GridRow>
+						<GridColumn width={8}></GridColumn>
+						<GridColumn width={4}>
+							<Form style={{ width: '50%' }}>
+								<FormInput />
+								<FormInput />
+								<Button primary>Log In</Button>
+							</Form>
+						</GridColumn>
+					</GridRow>
+				</Grid>
+			</Container>
+		</div>
+	);
 }
 
 export default LoginPage;
