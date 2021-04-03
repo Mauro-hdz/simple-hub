1a0e99c19d71802fe187049a34b01556f8ca2f43
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2021-04-02 21:17:24 -0500

    added email validation to create account

--- client/src/pages/CreateAccount.js
+++ client/src/pages/CreateAccount.js
@@ -7,10 +7,6 @@ import {
 	FormField,
 	FormInput,
 	Button,
-	Container,
-	GridColumn,
-	GridRow,
-	Grid,
 	Card,
 	CardHeader,
 	CardContent,
@@ -19,70 +15,74 @@ import {
 function CreateAccount() {
 	const history = useHistory();
 
-	const [userEmail, setUserEmail] = useState('');
-	const [userPasscode, setUserPasscode] = useState('');
+	const [userEmail, setUserEmail] = useState(null);
+	const [userPasscode, setUserPasscode] = useState(null);
 
-	async function onSubmitCreateAccount(e) {
-		const userInput = {
-			userEmail,
-			userPasscode,
-		};
+	const [invalidEmail, setInvalidEmail] = useState(false);
+	const [invalidPassword, setInvalidPassword] = useState(false);
 
-		const response = await axios.post(
-			'/api/user/create-account',
-			userInput
-		);
+	async function onSubmitCreateAccount() {
+		const validEmail = validateEmail(userEmail);
 
-		if (response) history.push('/');
+		if (validEmail) {
+			if (userPasscode != null) {
+				const userInput = {
+					userEmail,
+					userPasscode,
+				};
+
+				const response = await axios.post('/api/user/create-account', userInput);
+
+				if (response) history.push('/');
+			}
+		} else {
+			setInvalidEmail(true);
+		}
+	}
+
+	function validateEmail(email) {
+		const emailTest = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return emailTest.test(email.toLowerCase());
 	}
 
 	return (
-		<Container
-			style={{
-				paddingTop: '10%',
-				backgroundColor: 'lavender',
-				width: '100vw',
-				height: '100vh',
-			}}
-		>
-			<Grid centered>
-				<GridRow>
-					<GridColumn width={6}>
-						<Card style={{ padding: '5em', width: '100%' }}>
-							<CardHeader style={{ color: 'grey' }}>
-								Create Your Account
-							</CardHeader>
-							<CardContent>
-								<Form onSubmit={onSubmitCreateAccount}>
-									<FormField>
-										<FormInput
-											label='Email'
-											value={userEmail}
-											onChange={(e) =>
-												setUserEmail(e.target.value)
-											}
-										/>
-									</FormField>
-									<FormField>
-										<FormInput
-											label='Password'
-											value={userPasscode}
-											onChange={(e) =>
-												setUserPasscode(e.target.value)
-											}
-										/>
-									</FormField>
+		<div className='clearfix'>
+			<div className='col xs-col-4 xs-offset-4'>
+				<div className='xs-mt6'>
+					<Card style={{ padding: '5em', width: '100%' }}>
+						<CardHeader style={{ color: 'grey' }}>Create Your Account</CardHeader>
+						<CardContent>
+							<Form onSubmit={onSubmitCreateAccount}>
+								<FormField>
+									<FormInput
+										label='Email'
+										value={userEmail}
+										onChange={(e) => setUserEmail(e.target.value)}
+									/>
+									{invalidEmail && (
+										<span className='text-red'>Invalid Email!</span>
+									)}
+								</FormField>
+								<FormField>
+									<FormInput
+										label='Password'
+										value={userPasscode}
+										onChange={(e) => setUserPasscode(e.target.value)}
+									/>
+									{invalidPassword && (
+										<span className='text-red'>Invalid Password!</span>
+									)}
+								</FormField>
 
-									<Button type='submit' primary>
-										Log In
-									</Button>
-								</Form>
-							</CardContent>
-						</Card>
-					</GridColumn>
-				</GridRow>
-			</Grid>
-		</Container>
+								<Button type='submit' primary>
+									Create Account
+								</Button>
+							</Form>
+						</CardContent>
+					</Card>
+				</div>
+			</div>
+		</div>
 	);
 }
 
